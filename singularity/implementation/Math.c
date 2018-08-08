6afe64cb72af948cece4e5c4f40bed0e82abdc3d
ComdivByZero <ComdivByZero@yandex.ua>
2018-08-09 01:54:13 +0300

    Изменения Math.c для работы в условиях пересечения с math.h в WinAPI

--- singularity/implementation/Math.c
+++ singularity/implementation/Math.c
@@ -23,36 +23,59 @@
 #if (__STDC_VERSION__ >= 199901L) && !(defined(__TINYC__) && (defined(_WIN32) || defined(_WIN64)))
 	/* TODO */
 	enum { O7_C99 = 1 };
-	O7_INLINE double o7_log2 (double x) { return log2(x);  }
-	O7_INLINE double o7_asinh(double x) { return asinh(x); }
-	O7_INLINE double o7_acosh(double x) { return acosh(x); }
-	O7_INLINE double o7_atanh(double x) { return atanh(x); }
+	O7_INLINE double o7_log2 (double x) {
+		double log2(double);
+		return log2(x);
+	}
+	O7_INLINE double o7_asinh(double x) {
+		double asinh(double);
+		return asinh(x);
+	}
+	O7_INLINE double o7_acosh(double x) {
+		double acosh(double);
+		return acosh(x);
+	}
+	O7_INLINE double o7_atanh(double x) {
+		double atanh(double);
+		return atanh(x);
+	}
+	O7_INLINE double o7_round(double x) {
+		double round(double);
+		return round(x);
+	}
 #else
 	enum { O7_C99 = 0 };
 	O7_INLINE double o7_log2 (double x) { return O7_DBL_UNDEF; }
 	O7_INLINE double o7_asinh(double x) { return O7_DBL_UNDEF; }
 	O7_INLINE double o7_acosh(double x) { return O7_DBL_UNDEF; }
 	O7_INLINE double o7_atanh(double x) { return O7_DBL_UNDEF; }
+	O7_INLINE double o7_round(double x) { return O7_DBL_UNDEF; }
 #endif
 
 extern double Math_sqrt(double x) {
+	extern double sqrt(double);
 	assert(0.0 <= x);
 	return sqrt(x);
 }
 
 extern double Math_power(double x, double base) {
+	extern double pow(double, double);
 	return pow(o7_dbl(x), o7_dbl(base));
 }
 
 extern double Math_exp(double x) {
+	extern double exp(double);
 	return exp(o7_dbl(x));
 }
 
 extern double Math_ln(double x) {
+	extern double log(double);
 	return log(o7_dbl(x));
 }
 
 extern double Math_log(double x, double base) {
+	extern double log(double);
+	extern double log10(double);
 	double res;
 	if (O7_C99 && (base == 2.0)) {
 		res = o7_log2(o7_dbl(x));
@@ -68,46 +91,64 @@ extern double Math_log(double x, double base) {
 
 extern double Math_round(double x) {
 	/* TODO check */
-	return round(o7_dbl(x));
+	double res;
+
+	if (O7_C99) {
+		res = o7_round(x);
+	} else {
+		/* TODO */
+		assert(0 > 1);
+	}
+	return res;
 }
 
 extern double Math_sin(double x) {
+	extern double sin(double);
 	return sin(o7_dbl(x));
 }
 
 extern double Math_cos(double x) {
+	extern double cos(double);
 	return cos(o7_dbl(x));
 }
 
 extern double Math_tan(double x) {
+	extern double tan(double);
 	return tan(o7_dbl(x));
 }
 
 extern double Math_arcsin(double x) {
+	extern double asin(double);
 	return asin(o7_dbl(x));
 }
 
 extern double Math_arccos(double x) {
+	extern double acos(double);
 	return acos(o7_dbl(x));
 }
 
 extern double Math_arctan(double x) {
+	extern double atan(double);
 	return atan(o7_dbl(x));
 }
 
 extern double Math_arctan2(double x, double y) {
+	extern double atan2(double, double);
 	return atan2(o7_dbl(x), o7_dbl(y));
 }
 
 extern double Math_sinh(double x) {
+	extern double sinh(double);
 	return sinh(o7_dbl(x));
 }
 
 extern double Math_cosh(double x) {
+	extern double cosh(double);
 	return cosh(o7_dbl(x));
 }
 
 extern double Math_tanh(double x) {
+	extern double tanh(double);
 	return tanh(o7_dbl(x));
 }
 
--- test/source/MathTest.mod
+++ test/source/MathTest.mod
@@ -12,11 +12,15 @@ MODULE MathTest;
  BEGIN
    ASSERT(Math.sin(0.0) = 0.0);
    ASSERT(Math.sin(Math.pi) >= 0.0);
-   ASSERT(Math.sin(Math.pi / 2.0) = 1.0);
+   ASSERT(Math.sin(Math.pi) < 1.E-8);
+   ASSERT(1.0 <= Math.sin(Math.pi / 2.0));
+   ASSERT(Math.sin(Math.pi / 2.0) < 1.0 + 1.E-8);
 
-   ASSERT(Math.cos(0.0) = 1.);
+   ASSERT(Math.cos(0.0) >= 1.);
+   ASSERT(Math.cos(0.0) < 1. + 1.E-8);
    ASSERT(Math.cos(Math.pi) = -1.0);
-   ASSERT(Math.cos(Math.pi / 2.0) >= 0.)
+   ASSERT(-1.E-16 < Math.cos(Math.pi / 2.0));
+   ASSERT(Math.cos(Math.pi / 2.0) < 1.E-16)
  END Go;
 
 END MathTest.
