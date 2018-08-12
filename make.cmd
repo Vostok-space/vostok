c96f6155866e20df1eb6b8d02409369abfeeda68
ComdivByZero <ComdivByZero@yandex.ua>
2018-08-12 23:32:09 +0300

    Оставлена только раскруточная версия в первоначальной сборке

--- make.cmd
+++ make.cmd
@@ -15,24 +15,16 @@
 
 :FOUND
 
-@ECHO C compiler is %CC%
+@ECHO Compiler is %CC%
 
 @SET SING=..\..\singularity\bootstrap\singularity
 
 @MKDIR result\v0 result\v1 2>NUL
 
 @CD singularity\bootstrap
-%CC% Arithmetic.c Scanner.c Ast.c StringStore.c GeneratorC.c TextGenerator.c TypeLimits.c Translator.c TranslatorLimits.c Log.c Utf8.c Message.c MessageEn.c MessageRu.c V.c Out.c VDataStream.c Parser.c VFileStream.c PlatformExec.c CliParser.c singularity\CFiles.c singularity\CLI.c singularity\o7.c singularity\OsExec.c singularity\Platform.c singularity\OsEnv.c -I . -I singularity -o ..\..\result\bs-o7c.exe
+@%CC% Arithmetic.c Scanner.c Ast.c StringStore.c GeneratorC.c TextGenerator.c TypeLimits.c Translator.c TranslatorLimits.c Log.c Utf8.c Message.c MessageEn.c MessageRu.c V.c Out.c VDataStream.c Parser.c VFileStream.c PlatformExec.c CliParser.c singularity\CFiles.c singularity\CLI.c singularity\o7.c singularity\OsExec.c singularity\Platform.c singularity\OsEnv.c -I . -I singularity -o ..\..\result\bs-o7c.exe
 @CD ..\..
 
-result\bs-o7c to-c Translator.Start result\v0 -infr . -m source
-
-@CD result\v0
-
-%CC% CheckIntArithmetic.c Scanner.c Ast.c StringStore.c GeneratorC.c TextGenerator.c TypesLimits.c Translator.c TranslatorLimits.c Log.c Utf8.c Message.c MessageEn.c MessageRu.c MessageUa.c LocaleParser.c Chars0X.c V.c Out.c VDataStream.c Parser.c VFileStream.c PlatformExec.c CliParser.c OberonSpecIdent.c Utf8Transform.c SpecIdentChecker.c CCompilerInterface.c FileSystemUtil.c %SING%\CFiles.c %SING%\CLI.c %SING%\o7.c %SING%\OsExec.c %SING%\OsEnv.c %SING%\Platform.c -I . -I %SING% -o ..\o7c.exe
-@CD ..\..
-
-result\o7c to-bin Translator.Start result\v1\o7c.exe -infr . -m source -cc %CC%
-
-@ECHO Info about next steps:
+@ECHO:
+@ECHO Bootstrap version of translator was built. Info about next steps:
 @ECHO   result\bs-o7c run make.Help -infr . -m source
--- make.sh
+++ make.sh
@@ -5,8 +5,6 @@ SING_BS=singularity/bootstrap
 SANITIZE="-ftrapv -fsanitize=undefined -fsanitize=address -DO7_LSAN_LEAK_IGNORE"
 SANITIZE_TEST="$SANITIZE"
 O7_OPT="-DO7_MEMNG_MODEL=O7_MEMNG_NOFREE"
-#LD_OPT=-lgc
-LD_OPT=-lm
 WARN="-Wall -Wno-parentheses"
 DEBUG=-g
 OPTIM=-O1
@@ -17,5 +15,5 @@ CC_OPT="$WARN $OPTIM $DEBUG $O7_OPT $OPT"
 mkdir -p result
 $CC $CC_OPT $SANITIZE -I$SING_BS -I$SING_BS/singularity $SING_BS/*.c $SING_BS/singularity/*.c -o result/bs-o7c
 
-echo Info about next steps:
+echo Bootstrap version of translator was built. Info about next steps:
 echo "  result/bs-o7c run make.Help -infr . -m source"
