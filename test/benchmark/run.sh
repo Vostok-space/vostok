7c3f6c2c6ddcddce4e4ee615a62a23c472bc19b9
ComdivByZero <ComdivByZero@yandex.ua>
2018-11-21 02:50:11 +0200

    Использован ключи более оптимальных проверок gcc

--- test/benchmark/run.sh
+++ test/benchmark/run.sh
@@ -6,18 +6,19 @@ result/o7c to-c "RepeatTran.Go(10)" $RESULT/san  -infr . -m source -m test/bench
 
 export ASAN_OPTIONS=detect_odr_violation=0
 MAIN="gcc -Wno-logical-op-parentheses -O3 -flto -s -DO7_MEMNG_MODEL=O7_MEMNG_NOFREE -Isingularity/implementation singularity/implementation/*.c"
+TRAP="-fsanitize-undefined-trap-on-error -static-libasan"
 
-$MAIN -DNDEBUG -DO7_VAR_INIT_MODEL=O7_INIT_NO -I$RESULT/san $RESULT/san/*.c -o $RESULT/o7c
+$MAIN -DNDEBUG -DO7_VAR_INIT_MODEL=O7_INIT_NO -I$RESULT/san $RESULT/san/*.c -o $RESULT/o7c &
 
-$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -o $RESULT/o7c-asrt
+$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -o $RESULT/o7c-asrt &
 
-$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_NO -DNDEBUG -I$RESULT/san $RESULT/san/*.c -fsanitize=undefined -o $RESULT/o7c-usan
+$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_NO -DNDEBUG -I$RESULT/san $RESULT/san/*.c -fsanitize=undefined $TRAP -o $RESULT/o7c-usan &
 
-$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_NO -DNDEBUG -I$RESULT/san $RESULT/san/*.c -fsanitize=address -DO7_LSAN_LEAK_IGNORE -o $RESULT/o7c-asan
+$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_NO -DNDEBUG -I$RESULT/san $RESULT/san/*.c -fsanitize=address $TRAP -DO7_LSAN_LEAK_IGNORE -o $RESULT/o7c-asan &
 
-$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_NO -DNDEBUG -I$RESULT/san $RESULT/san/*.c -fsanitize=undefined -fsanitize=address -DO7_LSAN_LEAK_IGNORE -o $RESULT/o7c-uasan
+$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_NO -DNDEBUG -I$RESULT/san $RESULT/san/*.c -fsanitize=undefined -fsanitize=address $TRAP -DO7_LSAN_LEAK_IGNORE -o $RESULT/o7c-uasan
 
-$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -fsanitize=undefined -fsanitize=address -DO7_LSAN_LEAK_IGNORE -o $RESULT/o7c-uasan-asrt
+$MAIN -DO7_VAR_INIT_MODEL=O7_INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -fsanitize=undefined -fsanitize=address $TRAP -DO7_LSAN_LEAK_IGNORE -o $RESULT/o7c-uasan-asrt
 
 strip $RESULT/o7c*
 
