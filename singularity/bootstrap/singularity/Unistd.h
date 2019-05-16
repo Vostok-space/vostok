1a67b9783081684615e2e5f54e132fd741b5ae32
ComdivByZero <ComdivByZero@yandex.ua>
2019-05-17 02:38:06 +0300

    readlink to bootstrap

---
+++ singularity/bootstrap/singularity/Unistd.h
@@ -0,0 +1,9 @@
+#if !defined HEADER_GUARD_Unistd
+#    define  HEADER_GUARD_Unistd 1
+
+extern o7_int_t
+Unistd_Readlink(o7_int_t path_len, o7_char const pathname[O7_VLA(path_len)],
+                o7_int_t buf_len, o7_char buf[O7_VLA(buf_len)]);
+
+O7_ALWAYS_INLINE void Unistd_init(void) { ; }
+#endif
