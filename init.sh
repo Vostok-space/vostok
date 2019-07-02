337968641bc416997e1a434171180ab90adbd038
ComdivByZero <ComdivByZero@yandex.ua>
2019-07-03 01:45:56 +0300

    Добавлен поиск компилятора C

--- init.sh
+++ init.sh
@@ -9,13 +9,21 @@ WARN="-Wall -Wno-parentheses"
 DEBUG=-g
 OPTIM=-O1
 OPT=
-CC=cc
-CC_OPT="$WARN $OPTIM $DEBUG $O7_OPT $OPT"
+LD_OPT=-lm
+CC_OPT="$WARN $OPTIM $DEBUG $O7_OPT $OPT $LD_OPT $SANITIZE"
 
-echo C compiler is $CC
+for CC in cc gcc clang tcc; do
+    if $CC -v >/dev/null 2>/dev/null; then
+        echo C compiler is $CC
+        break
+    else if [ $CC = tcc ]; then
+        echo Can not found c compiler
+        exit 1
+    fi fi
+done
 
 mkdir -p result
-$CC $CC_OPT $SANITIZE -I$SING_BS -I$SING_BS/singularity $SING_BS/*.c $SING_BS/singularity/*.c -o result/bs-ost
+$CC $CC_OPT -I$SING_BS -I$SING_BS/singularity $SING_BS/*.c $SING_BS/singularity/*.c -o result/bs-ost
 
 echo Bootstrap version of translator was built. Info about next steps:
 echo "  result/bs-ost run make.Help -infr . -m source"
