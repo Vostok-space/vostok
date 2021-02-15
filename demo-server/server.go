4807f29193f5615cecde918ab21bd2faa7d10c3e
ComdivByZero <ComdivByZero@yandex.ua>
2021-02-15 02:44:33 +0200

    Telegram-bot

--- demo-server/server.go
+++ demo-server/server.go
@@ -1,5 +1,5 @@
 /*  HTTP server for translator demonstration
- *  Copyright (C) 2017-2018 ComdivByZero
+ *  Copyright (C) 2017-2019,2021 ComdivByZero
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published
@@ -27,10 +27,36 @@ import (
   "errors"
   "runtime"
   "time"
+  "encoding/json"
+  "bytes"
+  "strconv"
 )
 
 const (
-  kwModule = "MODULE"
+  kwModule = "MODULE";
+  teleApi = "https://api.telegram.org/bot"
+)
+
+type (
+  teleChat struct {
+    Id int `json:"id"`
+  }
+  teleMessage struct {
+    Chat teleChat `json:"chat"`;
+    Txt  string   `json:"text"`
+  }
+  teleUpdate struct {
+    Id  int         `json:"update_id"`;
+    Msg teleMessage `json:"message"`
+  }
+  teleUpdates struct {
+    Ok bool              `json:"ok"`;
+    Result []teleUpdate  `json:"result"`
+  }
+  teleAnswer struct {
+    Chat int    `json:"chat_id"`;
+    Txt  string `json:"text"`
+  }
 )
 
 func getModuleName(source string) (name string) {
@@ -44,7 +70,7 @@ func getModuleName(source string) (name string) {
       name = strings.TrimSpace(source[module: semicolon + module])
     }
   }
-  return name
+  return
 }
 
 func saveModule(name, source string) (tmp string, err error) {
@@ -58,9 +84,10 @@ func saveModule(name, source string) (tmp string, err error) {
       err = ioutil.WriteFile(filename, []byte(source), 0666);
     }
   } else {
+    tmp = "";
     err = errors.New("Can not found module name in source")
   }
-  return tmp, err
+  return
 }
 
 func run(source, script, cc string, timeout int) (output []byte, err error) {
@@ -68,8 +95,22 @@ func run(source, script, cc string, timeout int) (output []byte, err error) {
     name, tmp, bin, timeOut string;
     cmd *exec.Cmd
   )
-  name = getModuleName(source);
-  tmp, err = saveModule(name, source);
+  if source != "" {
+    name = getModuleName(source);
+    if name == "" && script == "" {
+      script = source;
+      source = "";
+    } else {
+      if script == "" {
+        script = name
+      }
+      tmp, err = saveModule(name, source)
+    }
+  }
+  if source == "" {
+    name = "script-module";
+    tmp, err = ioutil.TempDir(tmp, name)
+  }
   if err == nil {
     if runtime.GOOS == "windows" {
       bin = fmt.Sprintf("%v\\%v.exe", tmp, name)
@@ -99,7 +140,7 @@ func run(source, script, cc string, timeout int) (output []byte, err error) {
   } else {
     output = nil
   }
-  return output, err
+  return
 }
 
 func handler(w http.ResponseWriter, r *http.Request, cc string, timeout int) {
@@ -122,24 +163,114 @@ func handler(w http.ResponseWriter, r *http.Request, cc string, timeout int) {
   }
 }
 
-func main() {
-  var (
-    port, timeout *int;
-    cc *string;
-    err error
-  )
-  port    = flag.Int("port", 8080, "tcp/ip");
-  timeout = flag.Int("timeout", 5, "in seconds");
-  cc      = flag.String("cc", "tcc", "c compiler");
-  flag.Parse();
-
+func webServer(port, timeout int, cc string) (err error) {
   http.Handle("/", http.FileServer(http.Dir(".")));
   http.HandleFunc(
     "/run",
     func(w http.ResponseWriter, r *http.Request) {
-      handler(w, r, *cc, *timeout)
+      handler(w, r, cc, timeout)
     });
-  err = http.ListenAndServe(fmt.Sprintf(":%d", *port), nil);
+  return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func teleGetUpdates(api string, ofs int) (upd []teleUpdate, err error) {
+  var (
+    resp *http.Response;
+    data []byte;
+    teleUpd teleUpdates
+  )
+  resp, err = http.Get(api + "getUpdates?offset=" + strconv.Itoa(ofs));
+  if err == nil {
+    data, err = ioutil.ReadAll(resp.Body);
+    resp.Body.Close();
+
+    err = json.Unmarshal(data, &teleUpd);
+    if err == nil {
+      upd = teleUpd.Result
+    }
+  } else {
+    data = nil
+  }
+  return
+}
+
+func teleSend(api, text string, chat int) (err error) {
+  var (
+    resp *http.Response;
+    msg  teleAnswer;
+    data []byte
+  )
+  msg.Chat = chat;
+  msg.Txt = text;
+  data, err = json.Marshal(&msg);
+  if err == nil {
+    resp, err = http.Post(api + "sendMessage", "application/json", bytes.NewBuffer(data));
+    if err == nil {
+      resp.Body.Close()
+    }
+  }
+  return
+}
+
+func teleBot(token, cc string, timeout int) (err error) {
+  const (
+    help = `/O7: Out.String("Script mode")` +
+           "\n/MODULE ModuleMode; END ModuleMode.\n"
+  )
+  var (
+    api, src string;
+    upds []teleUpdate;
+    lastUpdate int;
+    output []byte;
+  )
+  api = teleApi + token + "/";
+  err = nil;
+  lastUpdate = -1;
+  for err == nil {
+    upds, err = teleGetUpdates(api, lastUpdate + 1);
+    if err == nil {
+      for _, upd := range upds {
+        src = upd.Msg.Txt;
+        if src[0] == '/' {
+          if strings.HasPrefix(src, "/O7:") {
+            src = src[4:]
+          } else if strings.HasPrefix(src, "/MODULE") {
+            src = src[1:]
+          } else {
+            src = ""
+          }
+        }
+        if src != "" {
+          output, err = run(src, "", cc, timeout);
+          err = teleSend(api, string(output), upd.Msg.Chat.Id)
+        } else if (upd.Msg.Txt == "/start") {
+          err = teleSend(api, help, upd.Msg.Chat.Id)
+        }
+        lastUpdate = upd.Id
+      }
+    }
+  }
+  return
+}
+
+func main() {
+  var (
+    port, timeout *int;
+    cc *string;
+    err error;
+    telegram *string
+  )
+  port     = flag.Int("port", 8080, "tcp/ip");
+  timeout  = flag.Int("timeout", 5, "in seconds");
+  cc       = flag.String("cc", "tcc", "c compiler");
+  telegram = flag.String("telegram", "", "telegram bot's token");
+  flag.Parse();
+
+  if *telegram != "" {
+    err = teleBot(*telegram, *cc, *timeout)
+  } else {
+    err = webServer(*port, *timeout, *cc)
+  }
   if err != nil {
     fmt.Println(err);
     os.Exit(1)
