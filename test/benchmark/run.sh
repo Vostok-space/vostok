76ad13d18cdee8c14be7606ea1e4aabc4d851e5a
ComdivByZero <ComdivByZero@yandex.ua>
2021-04-12 00:03:44 +0300

    Добавлено профилирование в benchmark

--- test/benchmark/run.sh
+++ test/benchmark/run.sh
@@ -1,48 +1,67 @@
+#!/bin/bash
+
 RESULT=result/benchmark
+GCDA=$RESULT/gcda
 
 mkdir -p $RESULT/asrt $RESULT/san
 result/ost to-c "RepeatTran.Go(10)" $RESULT/asrt -infr . -m source -m test/benchmark
-result/ost to-c "RepeatTran.Go(10)" $RESULT/san  -infr . -m source -m test/benchmark -init noinit
+result/ost to-c "RepeatTran.Go(10)" $RESULT/san  -infr . -m source -m test/benchmark -init noinit -no-array-index-check -no-nil-check -no-arithmetic-overflow-check
 
 export ASAN_OPTIONS=detect_odr_violation=0
 
 compile() {
-MAIN="gcc -O3 -flto -s -DO7_MEMNG_MODEL=O7_MEMNG_NOFREE -Isingularity/implementation singularity/implementation/o7.c singularity/implementation/CFiles.c singularity/implementation/CLI.c singularity/implementation/Platform.c singularity/implementation/OsEnv.c singularity/implementation/OsExec.c singularity/implementation/Unistd_.c"
+SI=singularity/implementation
+MAIN="gcc -O3 -flto -s -DO7_MEMNG_MODEL=O7_MEMNG_NOFREE -Isingularity/implementation $SI/o7.c $SI/CFiles.c $SI/CLI.c $SI/Platform.c $SI/OsEnv.c $SI/OsExec.c $SI/Unistd_.c $SI/CDir.c $SI/Wlibloaderapi.c"
 TRAP="-fsanitize-undefined-trap-on-error -static-libasan"
-USAN="-fsanitize=undefined -fno-sanitize=alignment"
+USAN="-fsanitize=undefined -fno-sanitize=alignment -fno-sanitize=null"
 ASAN="-fsanitize=address -DO7_LSAN_LEAK_IGNORE"
 INIT_NO="-DNDEBUG -DO7_INIT_MODEL=O7_INIT_NO"
 INIT_UNDEF="-DO7_INIT_MODEL=O7_INIT_UNDEF -DO7_ASSERT_NO_MESSAGE"
 
 NPROC=`nproc || sysctl -n hw.ncpu`
 
+PROFILE="$1 -fprofile-dir=$GCDA"
+
 # parallelize compilation
 echo \
-"$MAIN $INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -o $RESULT/ost-asrt" \
+"$MAIN $PROFILE-asrt $INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -o $RESULT/ost-asrt" \
 ,\
-"$MAIN $INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c $USAN $ASAN $TRAP -o $RESULT/ost-uasan-asrt" \
+"$MAIN $PROFILE-uasan-asrt $INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c $USAN $ASAN $TRAP -o $RESULT/ost-uasan-asrt" \
 ,\
-"$MAIN $INIT_NO -I$RESULT/san $RESULT/san/*.c $USAN $ASAN $TRAP -o $RESULT/ost-uasan" \
+"$MAIN $PROFILE-uasan $INIT_NO -I$RESULT/san $RESULT/san/*.c $USAN $ASAN $TRAP -o $RESULT/ost-uasan" \
 ,\
-"$MAIN $INIT_NO -I$RESULT/san $RESULT/san/*.c $ASAN $TRAP -o $RESULT/ost-asan" \
+"$MAIN $PROFILE-asan $INIT_NO -I$RESULT/san $RESULT/san/*.c $ASAN $TRAP -o $RESULT/ost-asan" \
 ,\
-"$MAIN $INIT_NO -I$RESULT/san $RESULT/san/*.c $USAN $TRAP -o $RESULT/ost-usan" \
+"$MAIN $PROFILE-usan $INIT_NO -I$RESULT/san $RESULT/san/*.c $USAN $TRAP -o $RESULT/ost-usan" \
 ,\
-"$MAIN $INIT_NO -I$RESULT/san $RESULT/san/*.c -o $RESULT/ost" \
+"$MAIN $PROFILE $INIT_NO -I$RESULT/san $RESULT/san/*.c -o $RESULT/ost" \
 | xargs -d , -n 1 -P $NPROC sh -c
 }
 
-compile
+run() {
+	LIST="ost ost-asrt ost-usan ost-asan ost-uasan ost-uasan-asrt"
 
-LIST="ost ost-asrt ost-usan ost-asan ost-uasan ost-uasan-asrt"
+	for ost in $LIST; do
+		echo
+		ls -l $RESULT/$ost
+		mkdir -p /tmp/ost-bench-$ost
+		for i in $@; do
+			/usr/bin/time -f "%e sec  %M KiB" $RESULT/$ost to-c "RepeatTran.Go(10)" /tmp/ost-bench-$ost -infr . -m source -m test/benchmark
+		done
+		crc32 <(cat /tmp/ost-bench-$ost/*)
+		rm -r /tmp/ost-bench-$ost
+	done
+}
 
-for ost in $LIST; do
-	echo
-	ls -l $RESULT/$ost
-	mkdir -p /tmp/ost-bench-$ost
-	for i in 0 1 2 3 4; do
-		/usr/bin/time -f "%e sec  %M KiB" $RESULT/$ost to-c "RepeatTran.Go(10)" /tmp/ost-bench-$ost -infr . -m source -m test/benchmark 2>&1
-	done;
-	crc32 <(cat /tmp/ost-bench-$ost/*)
-	rm -r /tmp/ost-bench-$ost
-done
+compile_and_profile() {
+	DIRS="$GCDA $GCDA-asrt $GCDA-usan $GCDA-asan $GCDA-uasan $GCDA-uasan-asrt"
+	rm -rf $DIRS
+	mkdir -p $DIRS
+
+	compile -fprofile-generate
+	run 0
+	compile -fprofile-use
+}
+
+compile_and_profile
+run 0 1 2 3 4
