9ad7451a38a283526b8566e3a1e7f4fc1f006b41
ComdivByZero <ComdivByZero@yandex.ua>
2021-04-26 23:13:06 +0300

    Профилирование для clang

--- test/benchmark/run.sh
+++ test/benchmark/run.sh
@@ -6,18 +6,26 @@ GCDA=$RESULT/gcda
 generate() {
 	rm -rf $RESULT
 	mkdir -p $RESULT/asrt $RESULT/san
-	result/ost to-c "RepeatTran.Go(10)" $RESULT/asrt -infr . -m source -m test/benchmark
-	result/ost to-c "RepeatTran.Go(10)" $RESULT/san  -infr . -m source -m test/benchmark \
-	           -init noinit -no-array-index-check -no-nil-check -no-arithmetic-overflow-check
+	SOURCE="-m source/blankJava -m source/blankOberon -m source/blankJs -m source/en-only -m source"
+	NOCHECK="-init noinit -no-array-index-check -no-nil-check -no-arithmetic-overflow-check"
+	result/ost to-c "RepeatTran.Go(10)" $RESULT/asrt -infr . $SOURCE -m test/benchmark
+	result/ost to-c "RepeatTran.Go(10)" $RESULT/san  -infr . $SOURCE -m test/benchmark $NOCHECK
 }
 
 export ASAN_OPTIONS=detect_odr_violation=0
 
+LIST_OST="ost ost-asrt ost-usan ost-asan ost-uasan ost-uasan-asrt"
+
 compile() {
 	SI=singularity/implementation
 	CFILES="$SI/o7.c $SI/CFiles.c $SI/CLI.c $SI/Platform.c $SI/OsEnv.c $SI/OsExec.c $SI/Unistd_.c $SI/CDir.c $SI/Wlibloaderapi.c"
-	MAIN="gcc -O3 -flto -s -DO7_MEMNG_MODEL=O7_MEMNG_NOFREE -Isingularity/implementation $CFILES"
-	TRAP="-fsanitize-undefined-trap-on-error -static-libasan"
+	CC=gcc
+	MAIN="$CC -O3 -flto -s -DO7_MEMNG_MODEL=O7_MEMNG_NOFREE -Isingularity/implementation $CFILES"
+	if [[ $CC == clang* ]]; then
+		TRAP="-fsanitize-undefined-trap-on-error"
+	else
+		TRAP="-fsanitize-undefined-trap-on-error -static-libasan"
+	fi
 	USAN="-fsanitize=undefined -fno-sanitize=alignment"
 	ASAN="-fsanitize=address -DO7_LSAN_LEAK_IGNORE"
 	INIT_NO="-DNDEBUG -DO7_INIT_MODEL=O7_INIT_NO"
@@ -26,34 +34,62 @@ compile() {
 	NPROC=`nproc || sysctl -n hw.ncpu`
 
 	PROFILE="$1 -fprofile-dir=$GCDA"
+	PROFILE_ASRT="$PROFILE-asrt"
+	PROFILE_UASAN_ASRT="$PROFILE-uasan-asrt"
+	PROFILE_UASAN="$PROFILE-uasan"
+	PROFILE_ASAN="$PROFILE-asan"
+	PROFILE_USAN="$PROFILE-usan"
+	if [[ $CC == clang* ]]; then
+		if [[ $1 == "-fprofile-generate" ]]; then
+			PROFILE=-fprofile-instr-generate
+			PROFILE_ASRT=$PROFILE
+			PROFILE_UASAN_ASRT=$PROFILE
+			PROFILE_UASAN=$PROFILE
+			PROFILE_ASAN=$PROFILE
+			PROFILE_USAN=$PROFILE
+		fi
+		if [[ $1 == "-fprofile-use" ]]; then
+			for ost in $LIST_OST; do
+				llvm-profdata merge $RESULT/$ost.profraw -output=$RESULT/$ost.profdata
+			done
+			PROFILE=-fprofile-instr-use=$RESULT/ost.profdata
+			PROFILE_ASRT=-fprofile-instr-use=$RESULT/ost-asrt.profdata
+			PROFILE_UASAN_ASRT=-fprofile-instr-use=$RESULT/ost-uasan-asrt.profdata
+			PROFILE_UASAN=-fprofile-instr-use=$RESULT/ost-uasan.profdata
+			PROFILE_ASAN=-fprofile-instr-use=$RESULT/ost-asan.profdata
+			PROFILE_USAN=-fprofile-instr-use=$RESULT/ost-usan.profdata
+		fi
+	fi
 
+	ASRT_SET="$INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c"
+	SAN_SET="$INIT_NO -I$RESULT/san $RESULT/san/*.c"
 	# parallelize compilation
 	echo \
-"$MAIN $PROFILE-asrt $INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c -o $RESULT/ost-asrt" \
-,\
-"$MAIN $PROFILE-uasan-asrt $INIT_UNDEF -I$RESULT/asrt $RESULT/asrt/*.c $USAN $ASAN $TRAP -o $RESULT/ost-uasan-asrt" \
-,\
-"$MAIN $PROFILE-uasan $INIT_NO -I$RESULT/san $RESULT/san/*.c $USAN $ASAN $TRAP -o $RESULT/ost-uasan" \
-,\
-"$MAIN $PROFILE-asan $INIT_NO -I$RESULT/san $RESULT/san/*.c $ASAN $TRAP -o $RESULT/ost-asan" \
-,\
-"$MAIN $PROFILE-usan $INIT_NO -I$RESULT/san $RESULT/san/*.c $USAN $TRAP -o $RESULT/ost-usan" \
-,\
-"$MAIN $PROFILE $INIT_NO -I$RESULT/san $RESULT/san/*.c -o $RESULT/ost" \
+	"$MAIN $PROFILE_ASRT $ASRT_SET -o $RESULT/ost-asrt" \
+	,\
+	"$MAIN $PROFILE_UASAN_ASRT $ASRT_SET $USAN $ASAN $TRAP -o $RESULT/ost-uasan-asrt" \
+	,\
+	"$MAIN $PROFILE_UASAN $SAN_SET $USAN $ASAN $TRAP -o $RESULT/ost-uasan" \
+	,\
+	"$MAIN $PROFILE_ASAN $SAN_SET $ASAN $TRAP -o $RESULT/ost-asan" \
+	,\
+	"$MAIN $PROFILE_USAN $SAN_SET $USAN $TRAP -o $RESULT/ost-usan" \
+	,\
+	"$MAIN $PROFILE $SAN_SET -o $RESULT/ost" \
 	| xargs -d , -n 1 -P $NPROC sh -c
 }
 
 run() {
-	LIST="ost ost-asrt ost-usan ost-asan ost-uasan ost-uasan-asrt"
-
-	for ost in $LIST; do
+	for ost in $LIST_OST; do
 		if [ -f $RESULT/$ost ]; then
 			echo
 			ls -l $RESULT/$ost
 			mkdir -p /tmp/ost-bench-$ost
 			for i in $@; do
-				/usr/bin/time -f "%e sec  %M KiB" $RESULT/$ost to-c "RepeatTran.Go(10)" \
-					/tmp/ost-bench-$ost -infr . -m source -m test/benchmark
+				LLVM_PROFILE_FILE="$RESULT/$ost.profraw" \
+				/usr/bin/time -f "%e sec  %M KiB" \
+					$RESULT/$ost to-c "RepeatTran.Go(10)" \
+						/tmp/ost-bench-$ost -infr . -m source -m test/benchmark
 			done
 			crc32 <(cat /tmp/ost-bench-$ost/*)
 			rm -r /tmp/ost-bench-$ost
@@ -63,11 +99,13 @@ run() {
 
 compile_with_profile() {
 	DIRS="$GCDA $GCDA-asrt $GCDA-usan $GCDA-asan $GCDA-uasan $GCDA-uasan-asrt"
-	rm -rf $DIRS
 	mkdir -p $DIRS
 
+	echo Compiles and runs for profiling
 	compile -fprofile-generate
 	run 0
+
+	echo Compiles using profiling
 	compile -fprofile-use
 }
 
